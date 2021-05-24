e346776a19bf79ec3a4a429f987511c5847bd275
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-05-24 09:50:10 +0300

    Change background color

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.firstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstcompose.ui.theme.FirstComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +31,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!")
+    }
+
 }
 
 @Preview(showBackground = true)
